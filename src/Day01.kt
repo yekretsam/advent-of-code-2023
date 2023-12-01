02729d3187139955d17627be29f2103c14114e43
Urbanitsch Thomas <yekretsam@gmail.com>
2023-12-01 19:38:49 +0100

    - day01 done <3

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,63 @@
+enum class Direction { First, Last }
+
 fun main() {
+    val spelledDigitsMap = hashMapOf(
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9')
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { str ->
+            "${str.first { it.isDigit() }}${str.last { it.isDigit() }}".toInt()
+        }
+    }
+
+
+    fun findMeMyDigit2(
+        inputString: String,
+        direction: Direction) : Char {
+        val digitIndex = when(direction) {
+            Direction.First -> inputString.indexOfFirst { it.isDigit() }
+            Direction.Last -> inputString.indexOfLast { it.isDigit() }
+        }
+        val wordPair = when(direction) {
+            Direction.First -> inputString.findAnyOf(spelledDigitsMap.keys)
+            Direction.Last -> inputString.findLastAnyOf(spelledDigitsMap.keys)
+        }
+
+        return if(wordPair != null && (digitIndex == -1 || when(direction) {
+                Direction.First -> wordPair.first < digitIndex
+                Direction.Last -> wordPair.first > digitIndex
+            })) {
+            spelledDigitsMap[wordPair.second]!!
+        } else {
+            inputString[digitIndex]
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { str ->
+            val first = findMeMyDigit2(str, Direction.First)
+            val last = findMeMyDigit2(str, Direction.Last)
+
+            // result for this row
+            "$first$last".toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test_part1")
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_test_part2")
+    check(part2(testInput2) == 281)
 
+    // solution
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
