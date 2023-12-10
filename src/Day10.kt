607b6ff0fb166eb9ac51292fb5944060f8bfca87
Urbanitsch Thomas <yekretsam@gmail.com>
2023-12-10 21:30:38 +0100

    - day10 part2 driving me mad, but i can draw it now :shrug:

--- src/Day10.kt
+++ src/Day10.kt
@@ -12,29 +12,24 @@ fun main() {
         throw IllegalStateException("S not found :(")
     }
 
-    fun isValidPath(input: List<String>, start: Pair<Int, Int>, toCheck: Pair<Int, Int>, includeS: Boolean) : Boolean {
-        val toCheckRelative = intArrayOf(
-            toCheck.first - start.first,
-            toCheck.second - start.second
-        )
-
+    fun isValidPath(input: List<String>, start: Pair<Int, Int>, toCheckRelative: Pair<Int, Int>, includeS: Boolean) : Boolean {
         // no diagonals
-        if(toCheckRelative.any { it == 0 }){
+        if(toCheckRelative.first == 0 || toCheckRelative.second == 0){
             val currChar = input[start.first][start.second]
-            val toCheckChar = input[toCheck.first][toCheck.second]
-            if(toCheckRelative[0] == -1) { // top
+            val toCheckChar = input[start.first + toCheckRelative.first][start.second + toCheckRelative.second]
+            if(toCheckRelative.first == -1) { // top
                 if(currChar == '|' || currChar == 'L' || currChar == 'J' || currChar == 'S') {
                     return toCheckChar == '|' || toCheckChar == '7' || toCheckChar == 'F' || (includeS && toCheckChar == 'S')
                 }
-            } else if(toCheckRelative[0] == 1) { // bottom
+            } else if(toCheckRelative.first == 1) { // bottom
                 if(currChar == '|' || currChar == '7' || currChar == 'F' || currChar == 'S') {
                     return toCheckChar == '|' || toCheckChar == 'L' || toCheckChar == 'J' || (includeS && toCheckChar == 'S')
                 }
-            } else if(toCheckRelative[1] == -1) { // left
+            } else if(toCheckRelative.second == -1) { // left
                 if(currChar == '-' || currChar == 'J' || currChar == '7' || currChar == 'S') {
                     return toCheckChar == '-' || toCheckChar == 'F' || toCheckChar == 'L' || (includeS && toCheckChar == 'S')
                 }
-            } else if(toCheckRelative[1] == 1) { // right
+            } else if(toCheckRelative.second == 1) { // right
                 if(currChar == '-' || currChar == 'F' || currChar == 'L' || currChar == 'S') {
                     return toCheckChar == '-' || toCheckChar == 'J' || toCheckChar == '7' || (includeS && toCheckChar == 'S')
                 }
@@ -45,6 +40,14 @@ fun main() {
         }
     }
 
+    fun Char.isPipe() : Boolean {
+        return this == 'S' || this == '║' || this == '═' || this == '╝' || this == '╗' || this == '╔' || this == '╚'
+    }
+
+    fun Char.isNotPipe() : Boolean {
+        return !this.isPipe()
+    }
+
     fun part1(input: List<String>) : Int {
         val startLocation = getStartCoords(input)
 
@@ -53,16 +56,20 @@ fun main() {
         currLocations.add(Pair(startLocation.first, startLocation.second))
         var steps = 0
 
+        val newMap: Array<CharArray> = Array(input.size) {
+            CharArray(input[0].length)
+        }
+
+        var lastLocation: Pair<Int, Int>? = null
         while(true) {
             val newLocations = arrayListOf<Pair<Int, Int>>()
             steps++
+            //println("currLocations: ${currLocations.size} (${currLocations.toList()})")
             currLocations.forEach { currLocation ->
+                // update my map
                 for (i in currLocation.first - 1..currLocation.first + 1) {
                     for (j in currLocation.second - 1..currLocation.second + 1) {
                         val checking = Pair(i, j)
-                        if (steps > 2 && input[currLocation.first][currLocation.second] == 'S') {
-                            return steps
-                        }
                         if (
                             // bound checks
                             i >= 0 && i < input.size &&
@@ -70,15 +77,83 @@ fun main() {
                             // skip explored Locations
                             !(explored.any { it.first == checking.first && it.second == checking.second })
                         ) {
-                            if (isValidPath(input, currLocation, Pair(i, j), steps > 2)) {
-                                explored.add(checking)
-                                newLocations.add(Pair(i, j))
+                            val toCheckRelative = Pair(
+                                i - currLocation.first,
+                                j - currLocation.second
+                            )
+                            if (isValidPath(input, currLocation, toCheckRelative, steps > 2)) {
+                                if(newLocations.size == 0) {
+                                    explored.add(checking)
+                                    newLocations.add(Pair(i, j))
+                                    if(lastLocation != null) {
+                                        val lastRelative = Pair(
+                                            lastLocation!!.first - currLocation.first,
+                                            lastLocation!!.second - currLocation.second
+                                        )
+                                        if(lastRelative.first == -1 && toCheckRelative.first == 1 ||
+                                            lastRelative.first == 1 && toCheckRelative.first == -1) {
+                                            newMap[currLocation.first][currLocation.second] = '║'
+                                        } else if(lastRelative.second == -1 && toCheckRelative.second == 1 ||
+                                            lastRelative.second == 1 && toCheckRelative.second == -1) {
+                                            newMap[currLocation.first][currLocation.second] = '═'
+                                        } else if(lastRelative.second == -1 && toCheckRelative.first == -1 ||
+                                            lastRelative.first == -1 && toCheckRelative.second == -1) {
+                                            newMap[currLocation.first][currLocation.second] = '╝'
+                                        } else if(lastRelative.second == -1 && toCheckRelative.first == 1 ||
+                                            lastRelative.first == 1 && toCheckRelative.second == -1) {
+                                            newMap[currLocation.first][currLocation.second] = '╗'
+                                        } else if(lastRelative.first == 1 && toCheckRelative.second == 1 ||
+                                            lastRelative.second == 1 && toCheckRelative.first == 1) {
+                                            newMap[currLocation.first][currLocation.second] = '╔'
+                                        } else if(lastRelative.first == -1 && toCheckRelative.second == 1 ||
+                                            lastRelative.second == 1 && toCheckRelative.first == -1) {
+                                            newMap[currLocation.first][currLocation.second] = '╚'
+                                        }
+                                    }
+                                    newMap[i][j] = 'S'
+                                    /*
+                                    if(toCheckRelative.first == -1) { // top
+                                        if(currLocation.second-1 >= 0 && newMap[currLocation.first-1][currLocation.second-1].isNotPipe()) { // topLeft
+                                            newMap[currLocation.first-1][currLocation.second-1] = '2'
+                                        } else if(currLocation.second+1 < input[0].length && newMap[currLocation.first-1][currLocation.second+1].isNotPipe()) { // topRight
+                                            newMap[currLocation.first-1][currLocation.second+1] = '3'
+                                        }
+                                    } else if(toCheckRelative.first == 1) { // bottom
+                                        if(currLocation.second-1 >= 0 && newMap[currLocation.first+1][currLocation.second-1].isNotPipe()) { // bottomLeft
+                                            newMap[currLocation.first+1][currLocation.second-1] = '3'
+                                        } else if(currLocation.second+1 < input[0].length && newMap[currLocation.first+1][currLocation.second+1].isNotPipe()) { // bottomRight
+                                            newMap[currLocation.first+1][currLocation.second+1] = '2'
+                                        }
+                                    } else if(toCheckRelative.second == -1) { // left
+                                        if(currLocation.first-1 >= 0 && newMap[currLocation.first-1][currLocation.second-1].isNotPipe()) { // leftTop
+                                            newMap[currLocation.first-1][currLocation.second-1] = '3'
+                                        } else if(currLocation.first+1 < input.size && newMap[currLocation.first+1][currLocation.second-1].isNotPipe()) { // leftBottom
+                                            newMap[currLocation.first+1][currLocation.second-1] = '2'
+                                        }
+                                    } else if(toCheckRelative.second == 1) { // right
+                                        if(currLocation.first-1 >= 0 && newMap[currLocation.first-1][currLocation.second+1].isNotPipe()) { // rightTop
+                                            newMap[currLocation.first-1][currLocation.second+1] = '2'
+                                        } else if(currLocation.first+1 < input.size && newMap[currLocation.first+1][currLocation.second+1].isNotPipe()) { // rightBottom
+                                            newMap[currLocation.first+1][currLocation.second+1] = '3'
+                                        }
+                                    }
+                                     */
+                                }
                             }
                         }
                     }
                 }
             }
-            if(newLocations.size == 0) return steps-1
+            if(newLocations.size == 0) {
+                newMap.forEachIndexed { rowIndex, row ->
+                    row.forEachIndexed { columnIndex, columns ->
+                        print(newMap[rowIndex][columnIndex])
+                    }
+                    println()
+                }
+                return (steps-1) / 2
+            }
+            lastLocation = currLocations[0]
             currLocations = newLocations
         }
     }
@@ -102,12 +177,5 @@ fun main() {
 
         val input2 = readInput("Day10")
         part1(input2).println()
-
-        //val testInput2 = readInput("Day10_test")
-        //check(part2(testInput2) == 1)
-
-        // solution
-        //part1(input).println()
-        //part2(input).println()
     }.also { println("This took us ${it.inWholeMilliseconds} ms") }
 }
